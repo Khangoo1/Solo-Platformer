1197be6aa3f4b41aa844974dc79cf0e6c5726850
Kangoo <kangoo.sauvage@gmail.com>
2020-10-16 17:01:45 +0200

    coding!

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -1,9 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerBehavior : MonoBehaviour
+
 {
+
+    private Inputs Inputs;
+    private Vector2 direction;
+
+    private void OnEnable()
+    {
+
+        Inputs = new Inputs();
+        Inputs.Enable();
+        Inputs.Player.Move.performed += OnMovePerformed;
+
+    }
+
+private void OnMovePerformed(InputAction.CallbackContext obj)
+{
+    direction = obj.ReadValue<Vector2>();
+    Debug.Log(direction);
+}
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -11,8 +35,11 @@ public class NewBehaviourScript : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        var myRigidBody = GetComponent<Rigidbody2D>();
+        direction.y = 0;
+        //myRigidBody.MovePosition(direction);
+        myRigidBody.velocity = direction;
     }
 }
