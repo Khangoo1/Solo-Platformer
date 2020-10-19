ea8e292e3a62898cc008f905e245865771a28581
Kangoo <kangoo.sauvage@gmail.com>
2020-10-19 11:56:34 +0200

    trying to make him J U M P

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -7,9 +7,15 @@ using UnityEngine.InputSystem;
 public class PlayerBehavior : MonoBehaviour
 
 {
+    [SerializeField] private float speed;
+    [SerializeField] private float maxSpeed;
+    [SerializeField] private float jumpForce;
 
     private Inputs Inputs;
     private Vector2 direction;
+    private Rigidbody2D myRigidBody;
+
+    private bool isOnGround = false;
 
     private void OnEnable()
     {
@@ -17,8 +23,20 @@ public class PlayerBehavior : MonoBehaviour
         Inputs = new Inputs();
         Inputs.Enable();
         Inputs.Player.Move.performed += OnMovePerformed;
+        Inputs.Player.Move.canceled += OnMoveCanceled;
+        Inputs.Player.Jump.performed += OnJumpPerformed;
 
     }
+
+
+private void OnJumpPerformed(InputAction.CallbackContext obj)
+{
+    if (isOnGround)
+    {
+        myRigidBody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+}
+
 
 private void OnMovePerformed(InputAction.CallbackContext obj)
 {
@@ -26,6 +44,11 @@ private void OnMovePerformed(InputAction.CallbackContext obj)
     Debug.Log(direction);
 }
 
+
+private void OnMoveCanceled(InputAction.CallbackContext obj)
+{
+    direction = Vector2.zero;
+}
 
 
     // Start is called before the first frame update
@@ -40,6 +63,8 @@ private void OnMovePerformed(InputAction.CallbackContext obj)
         var myRigidBody = GetComponent<Rigidbody2D>();
         direction.y = 0;
         //myRigidBody.MovePosition(direction);
-        myRigidBody.velocity = direction;
+        //myRigidBody.velocity = direction;
+        if (myRigidBody.velocity.sqrMagnitude < maxSpeed)
+            myRigidBody.AddForce(direction * speed);
     }
 }
