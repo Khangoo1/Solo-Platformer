dbd50bda6c3ff08bd35f39ba7916df6e01e1b990
Kangoo <kangoo.sauvage@gmail.com>
2020-10-21 12:23:49 +0200

    nO SCRIPT ERRORS IN MY CHRISTIAN PLATFORMER

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -9,7 +9,7 @@ public class PlayerBehavior : MonoBehaviour
     [SerializeField] private float jumpForce;
     [SerializeField] private LayerMask ground;
 
-    private Inputs Inputs;
+    //private Inputs Inputs;
     private Vector2 direction;
     private Rigidbody2D myRigidBody;
 
@@ -18,7 +18,7 @@ public class PlayerBehavior : MonoBehaviour
     private void OnEnable()
     {
 
-        Inputs = new Inputs();
+        var Inputs = new Inputs();
         Inputs.Enable();
         Inputs.Player.Move.performed += OnMovePerformed;
         Inputs.Player.Move.canceled += OnMoveCanceled;
@@ -53,23 +53,35 @@ private void OnMoveCanceled(InputAction.CallbackContext obj)
     // Start is called before the first frame update
     void Start()
     {
-        
+        myRigidBody = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        var myRigidBody = GetComponent<Rigidbody2D>();
-        direction.y = 0;
+        //var myRigidBody = GetComponent<Rigidbody2D>();
+        //direction.y = 0;
         //myRigidBody.MovePosition(direction);
         //myRigidBody.velocity = direction;
+        var PlayerDirection = new Vector2
+        {
+            x = direction.x,
+            y = 0
+        };
+
+
         if (myRigidBody.velocity.sqrMagnitude < maxSpeed)
+        { 
             myRigidBody.AddForce(direction * speed);
+        }
+            
     }
 
-    private void OnCollisionEnter(Collision other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
-        if (ground == (ground | (1 << other.gameObject.layer)))
+        var touchGround = ground == (ground | (1 << other.gameObject.layer));
+        var touchFromAbove = other.contacts[0].normal == Vector2.up;
+        if (touchGround && touchFromAbove)
         //if (other.gameObject.CompareTag("Ground") == true)
         {
             isOnGround = true;
